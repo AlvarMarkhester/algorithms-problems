e450bada740e51ba3e0aeafb0899aa1dd9308530
Alvar Markhester <alvarprivat@hotmail.com>
2022-08-21 00:57:37 +0200

    Create isSubsequence.js

---
+++ isSubsequence.js
@@ -0,0 +1,19 @@
+
+const s = "abc"
+const t = "ahbgdc"
+var isSubsequence = function(s, t) {
+    
+    let count = 0
+    
+    for(let i = 0; i < t.length; i++) {
+        if(t[i] === s[count] && count <= t.length) {
+            count++
+        }
+    }
+    
+    return count === s.length ? true : false
+    
+    
+};
+
+console.log(isSubsequence(s, t))
